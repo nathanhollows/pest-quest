5e8c7292808c4f88df0c3a41e11f084cbd3e78ea
nathanhollows <me@nathanhollows.com>
2022-06-27 16:55:50 +1200

    Fix structu

--- internal/domain/admin.go
+++ internal/domain/admin.go
@@ -5,6 +5,6 @@ import "gorm.io/gorm"
 // Admin stores logins for the admin panel
 type Admin struct {
 	gorm.Model
-	Username string `gorm:"index:idx_name,unique";json:"username"`
+	Username string `gorm:"index:idx_name,unique" json:"username"`
 	Password string `json:"password"`
 }
