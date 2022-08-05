bff02911e4556fe50a19bf8f7380437388a6994c
nathanhollows <me@nathanhollows.com>
2022-08-05 16:17:31 +1200

    Print the current IP address when the server starts

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net"
 	"net/http"
 	"os"
 	"path/filepath"
@@ -57,7 +59,21 @@ func main() {
 		&domain.MarkerType{},
 	)
 	routes()
+	fmt.Printf("%s%s%s", "http://", GetOutboundIP(), ":8803")
 	fmt.Println(http.ListenAndServe(":"+config.Cfg.Server.Port, router))
+}
+
+// Get preferred outbound ip of this machine
+func GetOutboundIP() net.IP {
+	conn, err := net.Dial("udp", "8.8.8.8:80")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+
+	localAddr := conn.LocalAddr().(*net.UDPAddr)
+
+	return localAddr.IP
 }
 
 // Set up the routes needed for the game.
